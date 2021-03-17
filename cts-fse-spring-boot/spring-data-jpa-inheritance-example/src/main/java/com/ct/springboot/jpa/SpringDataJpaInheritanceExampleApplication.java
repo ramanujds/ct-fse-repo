c17f68e32b5c9611f656a9d6c282126a832c1ce0
ramanujdas <ramanujds9@gmail.com>
2021-03-17 11:06:54 +0530

    Updated

---
+++ cts-fse-spring-boot/spring-data-jpa-inheritance-example/src/main/java/com/ct/springboot/jpa/SpringDataJpaInheritanceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ct.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaInheritanceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaInheritanceExampleApplication.class, args);
+	}
+
+}

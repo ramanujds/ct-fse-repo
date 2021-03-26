a900939d9fb0f18b93af53252f51b5685c7e9b79
ramanujdas <ramanujds9@gmail.com>
2021-03-26 17:04:59 +0530

    Updated

---
+++ cts-fse-spring-boot/spring-boot-image-upload/src/main/java/com/ct/springboot/imageupload/SpringBootImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.ct.springboot.imageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageUploadApplication.class, args);
+	}
+
+}
---
+++ cts-fse-spring-boot/spring-boot-image-upload/src/test/java/com/ct/springboot/imageupload/SpringBootImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.springboot.imageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

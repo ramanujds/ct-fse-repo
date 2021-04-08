40da01fea7eb50bf140cb8804364d06128279b05
ramanujdas <ramanujds9@gmail.com>
2021-04-08 10:53:14 +0530

    Added H2

---
+++ cts-fse-spring-boot/spring-rest-api-example/src/main/resources/data_2.sql
@@ -0,0 +1,3 @@
+insert into employee values (10004,'2000-05-15','gaurav@yahoo.com','Gaurav');
+insert into employee values (10005,'2001-06-15','rohit@yahoo.com','Rohit');
+insert into employee values (10006,'2002-10-10','harsh@yahoo.com','Harsh');
---
+++ cts-fse-spring-boot/spring-rest-api-example/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee values (10001,'2000-10-15','tushar@yahoo.com','Tushar');
+insert into employee values (10002,'2001-10-15','rajesh@yahoo.com','Rajesh');
+insert into employee values (10003,'2002-02-10','mahesh@yahoo.com','Mahesh');

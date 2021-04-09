8bd2c7f4949fd8118b8a42896cd4062d9b348a01
ramanujdas <ramanujds9@gmail.com>
2021-04-09 12:56:02 +0530

    Circuit Breaker

---
+++ cts-fse-spring-boot/microservices/recipe-service/src/test/java/com/ct/microservices/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

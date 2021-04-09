69dd8e33c311a721093db66892a3d991472dabbf
ramanujdas <ramanujds9@gmail.com>
2021-04-09 13:21:27 +0530

    Updated

--- cts-fse-spring-boot/microservices/recipe-service/src/main/java/com/ct/microservices/controller/RecipeController.java
+++ cts-fse-spring-boot/microservices/recipe-service/src/main/java/com/ct/microservices/controller/RecipeController.java
@@ -1,9 +1,11 @@
 package com.ct.microservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ct.microservices.model.Recipe;
@@ -24,31 +26,18 @@ public class RecipeController {
 	
 	
 	@GetMapping(value = "/item/{itemName}", produces = "application/json")
-	@HystrixCommand(fallbackMethod = "getRecipeFallback",
-			commandProperties = {
-					@HystrixProperty(name ="execution.timeout.enabled", value = "true" ),
-					@HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
-					@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value="2000"),
-					@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
-					@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
-					@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value="5000")
-									
-			}
-	
-			)
+
 	public RecipeMenu getRecipe(@PathVariable String itemName) {
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(10000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		int x=5/0;
 		return service.getRecipe(itemName);
 	}
 	
-	public RecipeMenu getRecipeFallback(@PathVariable String itemName) {
-		log.error("Recipe Service responded from Fallback");
-		return null;
-	}
+
 	
 }

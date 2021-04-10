5976f4756ce83744da6514f7b127da1d264bba13
ramanujdas <ramanujds9@gmail.com>
2021-04-10 16:56:59 +0530

    Added Zuul Filter and Spring Security

---
+++ cts-fse-spring-boot/microservices/food-app-api-gatewy/src/main/java/com/ct/microservices/controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.ct.microservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	
+}
---
+++ cts-fse-spring-boot/microservices/food-app-api-gatewy/src/test/java/com/ct/microservices/FoodAppApiGatewyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApiGatewyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
